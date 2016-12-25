2799edd4cf05ed60d9cd3fc74ce682c7eb779a55
Wiffzack <wiffzackius@gmail.com>
2016-12-25 18:16:15 +0100

    Update logscan.py

--- logscan.py
+++ logscan.py
@@ -1,13 +1,16 @@
-import mmap,os,sys,subprocess,time,socket
+import mmap,os,os.path,sys,subprocess,time,socket
 from subprocess import Popen
 import _winreg as wreg
-lookup = 'UDP Filtered Portscan'
+PATH= 'alert.ids'
 os.chdir(r'D:\Snort\log')
 line_number = line_num = counter = 0
 compare = ['[**]'];
-str1= ("cmd /c netsh advfirewall firewall add rule name=rule1 dir=in action=block protocol=any remoteip=")
-str2= ("127.0.0.1 ")
-str3= r'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\ZoneMap\\Domains\\'
+str1 = ("cmd /c netsh advfirewall firewall add rule name=rule1 dir=in action=block protocol=any remoteip=")
+str2 = ("127.0.0.1 ")
+str3 = r'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\ZoneMap\\Domains\\'
+str4 = r'SYSTEM\\CurrentControlSet\\Services\\TcpIp\\Parameters\\'
+str5 = r'SYSTEM\\CurrentControlSet\\services\\DNS\\Parameters\\'
+str6 = r'SYSTEM\\CurrentControlSet\\Services\\AFD\\Parameters\\'
 #line_number = int(raw_input('Enter the line number: '))
 x = []
 empty_line = []
@@ -19,6 +22,13 @@ def lookup(addr):
 	except socket.herror:
 		return None, None, None
 		
+def reverselookup(name):
+	try:
+		host = socket.gethostbyname(name)
+		return host
+	except socket.gaierror, err:
+		print "cannot resolve hostname: ", name, err
+
 def ipextract():
 	f = open('alert.ids', "U")
 	i = 1
@@ -27,15 +37,54 @@ def ipextract():
 			break
 		i += 1
 	words = line.split() 
-	str2= words[3]
+	str2= words[1]
 	str2 = str2.split(":", 1)[0]
+	f.close()
 	return str2
 	
-def winreg(dnsname):
+def dnscheck():
+	ip = ipextract()
+	print ip
+	name,alias,addresslist = lookup(ip)
+	ip2 = reverselookup(name)
+	winreg(name,'*',0x00000004,0,str3)
+	if ip in ip2:
+		pass
+	else:
+		print "Your DNS Cache is poisened!"
+		
+def synflood():
+	winreg(name,'SynAttackProtect',0x00000002,1,str4)
+	winreg(name,'TcpMaxPortsExhausted',0x00000005,1,str4)
+	winreg(name,'TcpMaxHalfOpen',0x000001F4,1,str4)
+	winreg(name,'TcpMaxHalfOpenRetried',0x00000190,1,str4)
+	
+def icmp():
+	winreg(name,'EnableICMPRedirect',0x00000000,1,str4)
+	
+def snmp():
+	winreg(name,'EnableDeadGWDetect',0x00000000,1,str4)
+	
+def additional():
+	winreg(name,'DisableIPSourceRouting',0x00000001,1,str4)
+	winreg(name,'EnableMulticastForwarding',0x00000000,1,str4)
+	winreg(name,'IPEnableRouter',0x00000000,1,str4)
+	winreg(name,'EnableAddrMaskReply',0x00000000,1,str4)
+	
+def afdprotection():
+	winreg(name,'EnableDynamicBacklog',0x00000001,1,str5)
+	winreg(name,'MinimumDynamicBacklog',0x00000014,1,str5)
+	winreg(name,'MaximumDynamicBacklog',0x00004E20,1,str5)
+	winreg(name,'DynamicBacklogGrowthDelta',0x0000000A,1,str5)
+	
+def winreg(dnsname, regn, rvalue,rpath,str):
 	regrule = "".join((str3, dnsname))
 	print regrule
-	key = wreg.CreateKey(wreg.HKEY_CURRENT_USER, regrule)
-	wreg.SetValueEx(key, '*', 0, wreg.REG_DWORD, 0x00000004)
+	if rpath == 0:
+		key = wreg.CreateKey(wreg.HKEY_CURRENT_USER, regrule)
+	else:
+		key = wreg.CreateKey(wreg.HKEY_LOCAL_MACHINE, regrule)
+	wreg.SetValueEx(key, regn, 0, wreg.REG_DWORD, rvalue)
 
 def firewallr():
 	global counter
@@ -65,30 +114,39 @@ def file_len(fname):
 	f.close()
 	return i + 1
 
-	
-while run:	
-	last_line = file_len('alert.ids')
-	myFile = open('alert.ids', "U")
-	for line in myFile.readlines():
-		line_num += 1
-		emptys()
-		if line_num == last_line:
-			time.sleep(1)
-			break
-		if line.find(compare[0]) >= 0:
-			line_nums=str(line_num)
-			if any(line_nums in s for s in x):
-				pass
-			else:
-				x.extend([line_nums])
-				if 'portscan' in line:
-					line_number = line_num+2
-					print "easy"
-					firewallr()
-				if 'OBFUSCATION' in line:
-					line_number = line_num+2
-					name,alias,addresslist = lookup(ipextract())
-					winreg(name)			
-	time.sleep(1)
-	myFile.close()
-	line_num = 0
+if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
+	while run:	
+		last_line = file_len('alert.ids')
+		myFile = open('alert.ids', "U")
+		for line in myFile.readlines():
+			line_num += 1
+			emptys()
+			if line_num == last_line:
+				time.sleep(1)
+				break
+			if line.find(compare[0]) >= 0:
+				line_nums=str(line_num)
+				if any(line_nums in s for s in x):
+					pass
+				else:
+					x.extend([line_nums])
+					if 'portscan' in line:
+						line_number = line_num+2
+						firewallr()
+					if 'OBFUSCATION' in line:
+						line_number = line_num+2
+						dnscheck()
+					if 'flood' in line:
+						line_number = line_num+2
+						synflood()
+					if 'ddos' in line:
+						line_number = line_num+2
+						icmp()
+					if 'snmp' in line:
+						line_number = line_num+2
+						snmp()
+		time.sleep(1)
+		myFile.close()
+		line_num = 0
+else:
+	print "Either file is missing or is not readable"
